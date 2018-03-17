db1e3f8f7e32fb52927fbe2d1dadb60887176750
Suapkit Thanomboonchareon <t.supakit@hotmail.com>
2018-03-17 11:02:05 +0700

    Add deployment files.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCs2NEgHP3U0JXaKnhRI7_2l80n9qiOFZU",
+    authDomain: "ravipanich-optic.firebaseapp.com",
+    databaseURL: "https://ravipanich-optic.firebaseio.com",
+    projectId: "ravipanich-optic",
+    storageBucket: "ravipanich-optic.appspot.com",
+    messagingSenderId: "92362886527"
+  }
 };
